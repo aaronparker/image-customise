c39c79792f081c57c425c6808b701253ca2ea801
Aaron Parker <aaron@stealthpuppy.com>
2020-05-17 21:13:02 +1000

    REM transcript

--- Invoke-Scripts.ps1
+++ Invoke-Scripts.ps1
@@ -14,7 +14,7 @@ Param ()
 $stampDate = Get-Date
 $scriptName = ([System.IO.Path]::GetFileNameWithoutExtension($(Split-Path $script:MyInvocation.MyCommand.Path -Leaf)))
 $logFile = "$env:SystemRoot\Logs\$scriptName-" + $stampDate.ToFileTimeUtc() + ".log"
-Start-Transcript -Path $logFile
+#Start-Transcript -Path $logFile
 
 # Get system properties
 Switch -Regex ((Get-WmiObject Win32_OperatingSystem).Caption) {
@@ -55,4 +55,4 @@ ForEach ($script in ($AllScripts + $PlatformScripts + $BuildScripts + $ModelScri
 }
 
 # End log
-Stop-Transcript
+#Stop-Transcript
