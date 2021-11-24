fbac46038bb4c5cb887adc47dcfbe627ef1ea902
Aaron Parker <aaron@stealthpuppy.com>
2021-11-25 08:30:14 +1100

    Update Install.ps1

--- ci/Install.ps1
+++ ci/Install.ps1
@@ -34,16 +34,16 @@ Write-Host "PowerShell Version:" $PSVersionTable.PSVersion.ToString()
 
 # Install packages
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.208
-If (Get-PSRepository -Name PSGallery | Where-Object { $_.InstallationPolicy -ne "Trusted" }) {
-    Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+Install-PackageProvider -Name "NuGet" -MinimumVersion "2.8.5.208" -Force -ErrorAction "SilentlyContinue"
+If (Get-PSRepository -Name "PSGallery" | Where-Object { $_.InstallationPolicy -ne "Trusted" }) {
+    Set-PSRepository -Name "PSGallery" -InstallationPolicy "Trusted"
 }
 If ([Version]((Find-Module -Name Pester).Version) -gt (Get-Module -Name Pester).Version) {
-    Install-Module -Name Pester -SkipPublisherCheck -Force -MaximumVersion 4.10.1
+    Install-Module -Name "Pester" -SkipPublisherCheck -Force -MaximumVersion "4.10.1"
 }
 If ([Version]((Find-Module -Name PSScriptAnalyzer).Version) -gt (Get-Module -Name PSScriptAnalyzer).Version) {
-    Install-Module -Name PSScriptAnalyzer -SkipPublisherCheck -Force
+    Install-Module -Name "PSScriptAnalyzer" -SkipPublisherCheck -Force
 }
 If ([Version]((Find-Module -Name posh-git).Version) -gt (Get-Module -Name posh-git).Version) {
-    Install-Module -Name posh-git -Force
+    Install-Module -Name "posh-git" -Force
 }
