31faca210953ad31a8c8bfc21ae8732ba020fb60
Aaron Parker <aaron@stealthpuppy.com>
2025-04-15 08:44:31 +1000

    Update success.cmd
    remove support for x86, call explicit script path and arguments

--- src/success.cmd
+++ src/success.cmd
@@ -1,7 +1,4 @@
 @ECHO OFF
-cd /d %~dp0
-if %PROCESSOR_ARCHITECTURE% == x86 (
-    %SystemRoot%\Sysnative\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy Bypass -NoLogo -NonInteractive -WindowStyle Hidden -File ".\Install-Defaults.ps1"
-) else (
-    %SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy Bypass -NoLogo -NonInteractive -WindowStyle Hidden -File ".\Install-Defaults.ps1"
-)
+SET SOURCE=C:\WINDOWS\System32\update\run\f38de27b-799e-4c30-8a01-bfdedc622944
+%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy Bypass -NoLogo -NonInteractive -WindowStyle Hidden -File "%SOURCE%\Remove-AppxApps.ps1" -Targeted
+%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy Bypass -NoLogo -NonInteractive -WindowStyle Hidden -File "%SOURCE%\Install-Defaults.ps1" -Path "%SOURCE%"
